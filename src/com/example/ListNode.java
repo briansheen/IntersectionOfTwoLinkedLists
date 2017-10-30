07d7a46c86c3c2151b33ea984c7f2213363651df
Brian Sheen <bsheen@gmail.com>
2017-10-30 15:18:21 -0500

    initial commit

---
+++ src/com/example/ListNode.java
@@ -0,0 +1,21 @@
+package com.example;
+
+/**
+ * Created by bsheen on 10/30/17.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+}
---
+++ src/com/example/Main.java
@@ -0,0 +1,50 @@
+package com.example;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ListNode a1 = new ListNode(1);
+        ListNode a2 = new ListNode(3);
+        ListNode b1 = new ListNode(2);
+        ListNode b2 = new ListNode(4);
+        ListNode b3 = new ListNode(5);
+        ListNode c1 = new ListNode(6);
+        ListNode c2 = new ListNode(7);
+        a1.next = a2;
+        a2.next = c1;
+        b1.next = b2;
+        b2.next = b3;
+        b3.next = c1;
+        c1.next = c2;
+        System.out.println(getIntersectionNode(a1,b1));
+    }
+
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> visitedNodes = new HashSet<>();
+        if(headA == null || headB == null){
+            return null;
+        }
+        while (headA != null || headB != null) {
+            if (visitedNodes.contains(headA)) {
+                return headA;
+            } else {
+                if (headA != null) {
+                    visitedNodes.add(headA);
+                    headA = headA.next;
+                }
+            }
+            if (visitedNodes.contains(headB)) {
+                return headB;
+            } else {
+                if (headB != null) {
+                    visitedNodes.add(headB);
+                    headB = headB.next;
+                }
+            }
+        }
+        return null;
+    }
+}
